0f341999e540754f0539cf732dc06ccf9157bee6
luisarce27 <larcep106@ulacit.ed.cr>
2018-12-12 22:40:24 -0600

    rev

---
+++ Taller1/Comentario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller1
+{
+    class Comentario
+    {
+        public DateTime FechaComenterio { get; set; }
+        public string Autor { get; set; }
+        public string Contenido { get; set; }
+        public int NumeroComentario { get; set; }
+        private Publicacion Comentarios { get; set; }
+
+
+        public void AgregarComentario()
+        {
+            Comentarios = new List<Publicacion>
+            {
+                new Publicacion(){Autor= "Jose", Comentarios= , Contenido="sdfhrjhferf",FechaPublicacion=15/1/2018,NumeroPublicacion=15/1/2018}
+
+            };
+
+        }
+    }
+}
--- Taller1/Program.cs
+++ Taller1/Program.cs
@@ -10,6 +10,38 @@ namespace Taller1
     {
         static void Main(string[] args)
         {
+            Usuario usuario = new Usuario();
+            Publicacion publicacion = new Publicacion();
+
+
+            int select = 0;
+            Console.WriteLine("Tipo de usuario 1- Editor 2-Lector");
+            while (select != 3)
+            {
+               
+                string cadselec;
+
+                cadselec = Console.ReadLine();
+
+                select = Convert.ToInt32(cadselec);
+
+                if (select == 1)
+                {
+                    Console.WriteLine("Presione 3 para salir");
+                    Console.WriteLine("Usuario seleccionado es usuario ");
+
+                   
+
+                }else if(select == 2){
+
+                    Console.WriteLine("Usuario tipo Lector puede agregar un comentario a la publicacion");
+                }
+
+            }
+
+            
+
+
         }
     }
 }
